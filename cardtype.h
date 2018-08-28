2765f75baffd13219e0e696ad7632cf940af4479
Jonathan Cox <jcox@netburner.com>
2018-08-28 14:38:40 -0700

    - Updated copyright information

--- cardtype.h
+++ cardtype.h
@@ -1,21 +1,42 @@
-/*NB_REVISION*/
+/* Revision: 2.8.7 */
 
-/*NB_COPYRIGHT*/
-
+/******************************************************************************
+* Copyright 1998-2018 NetBurner, Inc.  ALL RIGHTS RESERVED
+*
+*    Permission is hereby granted to purchasers of NetBurner Hardware to use or
+*    modify this computer program for any use as long as the resultant program
+*    is only executed on NetBurner provided hardware.
+*
+*    No other rights to use this program or its derivatives in part or in
+*    whole are granted.
+*
+*    It may be possible to license this or other NetBurner software for use on
+*    non-NetBurner Hardware. Contact sales@Netburner.com for more information.
+*
+*    NetBurner makes no representation or warranties with respect to the
+*    performance of this computer program, and specifically disclaims any
+*    responsibility for any damages, special or consequential, connected with
+*    the use of this program.
+*
+* NetBurner
+* 5405 Morehouse Dr.
+* San Diego, CA 92121
+* www.netburner.com
+******************************************************************************/
 
 /* Select the card type */
 #ifndef _CARDTYPE_H
 #define _CARDTYPE_H
 
-/*
-  Change between devices by uncommenting one or the other. You cannot
-  use both types at the same time. Whenever you change card types, you
-  should do a make clean on your project.
-
-  Warning! You must have CFC bus interface hardware on your platform or
-  the code will repeatedly trap. If you are getting traps you will need to
-  perform an application download using the monitor program to recover.
-*/
+/**
+ * Change between devices by uncommenting one or the other. You cannot
+ * use both types at the same time. Whenever you change card types, you
+ * should do a make clean on your project.
+ *
+ * Warning! You must have CFC bus interface hardware on your platform or
+ * the code will repeatedly trap. If you are getting traps you will need to
+ * perform an application download using the monitor program to recover.
+ */
 #define USE_MMC         // SD/MMC cards
 //#define USE_CFC       // Compact Flash cards
 //#define USE_RAM       // RAM FileSystem, See EFFS-RAM-minimal for details
@@ -28,4 +49,5 @@
 #endif
 
 #endif   /* _CARDTYPE_H */
+
 
--- web.h
+++ web.h
@@ -1,6 +1,28 @@
-/*NB_REVISION*/
+/* Revision: 2.8.7 */
 
-/*NB_COPYRIGHT*/
+/******************************************************************************
+* Copyright 1998-2018 NetBurner, Inc.  ALL RIGHTS RESERVED
+*
+*    Permission is hereby granted to purchasers of NetBurner Hardware to use or
+*    modify this computer program for any use as long as the resultant program
+*    is only executed on NetBurner provided hardware.
+*
+*    No other rights to use this program or its derivatives in part or in
+*    whole are granted.
+*
+*    It may be possible to license this or other NetBurner software for use on
+*    non-NetBurner Hardware. Contact sales@Netburner.com for more information.
+*
+*    NetBurner makes no representation or warranties with respect to the
+*    performance of this computer program, and specifically disclaims any
+*    responsibility for any damages, special or consequential, connected with
+*    the use of this program.
+*
+* NetBurner
+* 5405 Morehouse Dr.
+* San Diego, CA 92121
+* www.netburner.com
+******************************************************************************/
 
 #ifndef _WEB_H_
 #define _WEB_H_
